ab94a4476cc99e343943e487e83880faf0e794af
Wagner Pompeo <wag_pompeo@yahoo.com.br>
2026-01-24 10:34:18 -0300

    New project Shape Geometrics

---
+++ ControllerEmployee/src/application/ProgramGeometrics.java
@@ -0,0 +1,57 @@
+package application;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.Shape;
+import entities.Rectangle;
+import entities.Circle;
+import entities.enums.Color;
+
+public class ProgramGeometrics {
+
+	public static void main(String[] args) {
+
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+
+		List<Shape> list = new ArrayList<>();
+
+		System.out.print("Enter the number of shapes: ");
+		int numberShapes = sc.nextInt();
+
+		for (int i = 1; i <= numberShapes; i++) {
+			System.out.println("Shape #" + i + " data ");
+			System.out.print("Rectangle or Circle (r/c)?");
+			char typeShape = sc.next().charAt(0);
+			System.out.print("Color? (Black/Blue/Red): ");
+			Color color = Color.valueOf(sc.next());
+
+			if (typeShape == 'r') {
+				System.out.print("Width: ");
+				double width = sc.nextDouble();
+				System.out.print("Height: ");
+				double height = sc.nextDouble();
+				list.add(new Rectangle(color, width, height));
+			} else {
+				System.out.print("Radius: ");
+				double radius = sc.nextDouble();
+				list.add(new Circle(color, radius));
+
+			}
+
+		}
+		System.out.println();
+		System.out.println("SHAPE AREAS:");
+		for (Shape shape : list) {
+			System.out.println(String.format("%.2f", shape.area()));
+		}
+		
+
+		sc.close();
+
+	}
+
+}
